02a915c55678f0f021481450c8a15dbe1f27ed9c
Tor Hagland <torhagl@gmail.com>
2017-12-22 01:51:16 +0100

    Forgot to fix namespace when moving examplemodel to own file.

--- ExampleModels.elm
+++ ExampleModels.elm
@@ -1,10 +1,10 @@
 module ExampleModels exposing (..)
 
-import DEMO_S5 exposing (EpistM)
+import DEMO_S5 as Demo exposing (EpistM)
 import Dict
 import Prop
 
 
 exampleModel : EpistM
 exampleModel =
-    Mo [ 0, 1 ] [ "a", "b" ] (Dict.fromList [ ( "a", [ [ 0 ], [ 1 ] ] ), ( "b", [ [ 0, 1 ] ] ) ]) (Dict.fromList [ ( 0, [ Prop.p, Prop.r, Prop.s ] ), ( 1, [ Prop.q ] ) ]) 1
+    Demo.Mo [ 0, 1 ] [ "a", "b" ] (Dict.fromList [ ( "a", [ [ 0 ], [ 1 ] ] ), ( "b", [ [ 0, 1 ] ] ) ]) (Dict.fromList [ ( 0, [ Prop.p, Prop.r, Prop.s ] ), ( 1, [ Prop.q ] ) ]) 1
